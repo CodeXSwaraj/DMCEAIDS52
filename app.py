f5b5387044de42044462983f14d9ba610c6c9197
CodeXSwaraj <150808930+CodeXSwaraj@users.noreply.github.com>
2024-01-12 15:38:31 +0530

    Update app.py
    dfgdhf

--- app.py
+++ app.py
@@ -1,33 +1,12 @@
-# game_app.py
+# app.py
 
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
-import random
-
-# Generate a random number between 1 and 10
-secret_number = random.randint(1, 10)
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        guess = int(request.form['guess'])
-        return check_guess(guess)
-    return """
-    <h1>Guess the Number</h1>
-    <form method="post" action="/">
-        <label for="guess">Enter your guess (1-10):</label>
-        <input type="number" name="guess" required min="1" max="10">
-        <button type="submit">Submit Guess</button>
-    </form>
-    """
-
-def check_guess(guess):
-    if guess == secret_number:
-        return "Congratulations! You guessed the correct number. <a href='/'>Play Again</a>"
-    else:
-        return "Wrong guess. Try again! <a href='/'>Go Back</a>"
+    return 'Hello, Azure! This is a simple Flask app hosted on Azure.'
 
 if __name__ == '__main__':
     app.run(debug=True)
