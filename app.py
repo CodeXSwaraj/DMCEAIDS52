3961598ae28ee291980cf17aa6c1a3fa9acc7e67
CodeXSwaraj <150808930+CodeXSwaraj@users.noreply.github.com>
2024-01-13 15:34:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,4 +7,6 @@ def home():
     return 'Hello, Azure! This is a simple Flask app hosted on not Azure.'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Run the app on port 5000
+    app.run(debug=True, port=5000)
+
